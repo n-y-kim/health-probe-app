8fdc8886156c373a4cd242eb6c8df55a68a0ff2e
n-y-kim <nyn2265@gmail.com>
2023-12-15 07:27:49 +0900

    Refactor: Wrap functions as Handler Class

--- vmlifecyclehandler.py
+++ vmlifecyclehandler.py
@@ -10,50 +10,51 @@ import requests, json, os
 vmInstance = InstanceMetadata().populate()
 logger.info(vmInstance)
 
-"""
-    Here is where we need to put all the custom tasks that need to be performed
-"""
-def  performCustomOperation():
-    logger.info("Performing custom operation")
+class VMLifecycleHandler:
+    """
+        Here is where we need to put all the custom tasks that need to be performed
+    """
+    def  performCustomOperation():
+        logger.info("Performing custom operation")
 
-    ## This is where the custom logic will go
+        ## This is where the custom logic will go
 
 
-"""
-This will call the health Probe URL and fail it
-"""
-def failLoadBalancerProbes():
-    logger.info("Failing Health Probes")
-    try:
-        kill_health_probe = config.get('shell-commands', 'kill_health_probe_process')
-         # Delete all cron jobs
-        kill_process = os.system(kill_health_probe)
+    """
+    This will call the health Probe URL and fail it
+    """
+    def failLoadBalancerProbes():
+        logger.info("Failing Health Probes")
+        try:
+            kill_health_probe = config.get('shell-commands', 'kill_health_probe_process')
+            # Delete all cron jobs
+            kill_process = os.system(kill_health_probe)
 
-        if kill_process is not 0:
-            logger.error("Error killing health probe")
-    except:
-        logger.error("Error in failing health probe")
+            if kill_process is not 0:
+                logger.error("Error killing health probe")
+        except:
+            logger.error("Error in failing health probe")
 
-"""
-This will kill the cron job which collects and submits custom metric to Azure Monitor
-"""
-def stopCustomMetricFlow():
-    logger.info("Stopping the Custom Metrics")
-    removeCrontab = config.get('shell-commands', 'remove_all_crontab')
+    """
+    This will kill the cron job which collects and submits custom metric to Azure Monitor
+    """
+    def stopCustomMetricFlow():
+        logger.info("Stopping the Custom Metrics")
+        removeCrontab = config.get('shell-commands', 'remove_all_crontab')
 
-    #removeCrontab = "crontab -r"
+        #removeCrontab = "crontab -r"
+        
+        logger.info("Deleting all cron jobs")
     
-    logger.info("Deleting all cron jobs")
-   
-    # Delete all cron jobs
-    areCronsRemoved = os.system(removeCrontab)
+        # Delete all cron jobs
+        areCronsRemoved = os.system(removeCrontab)
 
-    if areCronsRemoved is not 0:
-        logger.error("Error deleting Cron jobs, health probe will not fail")
+        if areCronsRemoved is not 0:
+            logger.error("Error deleting Cron jobs, health probe will not fail")
 
-if(vmInstance.isPendingDelete()):
-    logger.info("Pending Delete is true ...starting custom clean up logic")
-    stopCustomMetricFlow()
-    performCustomOperation()
-else: 
-    logger.info("Instance not in Pending Delete, nothing to do")
+    if(vmInstance.isPendingDelete()):
+        logger.info("Pending Delete is true ...starting custom clean up logic")
+        stopCustomMetricFlow()
+        performCustomOperation()
+    else: 
+        logger.info("Instance not in Pending Delete, nothing to do")
